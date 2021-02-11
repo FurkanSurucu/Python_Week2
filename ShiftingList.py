933e4ffed4abc2702b53866a948c3ce47006c932
FurkanSurucu <77921425+FurkanSurucu@users.noreply.github.com>
2021-02-11 16:45:30 +0200

    Update ShiftingList.py

--- ShiftingList.py
+++ ShiftingList.py
@@ -4,7 +4,7 @@ Write a program that takes two inputs; one of them is a list and the other is a 
 and returns the list obtained by shifting the elements in the list n places to the right (left) when n is positive (negative).
 Use wrap-around: if an element is shifted beyond the end of the list, then continue to shift starting at the beginning of the list.
 '''
-elements = list(input("Enter list elements:\n"))
+elements = list(input('Enter list elements:\nPlease put comma for each elements\n').split(","))
 number= int(input('''Enter a number to rotate your list:
 To the right (positive)
 To the left (negative)
